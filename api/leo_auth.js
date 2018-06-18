acc8d293eaaeec8b3e687b00ef1857799b8bd9b3
Steve <steve@roaringdevelopment.com>
2018-06-18 11:39:24 -0600

    working in the new leo-auth

---
+++ api/leo_auth.js
@@ -0,0 +1,46 @@
+module.exports = {
+	/**
+	 * All actions will be prefixed with this domain, unless they contain a :
+	 **/
+	actions: 'templateCache',
+	/**
+	 * All resources belong to this resource group, unless they start with lrn
+	 **/
+	resource: 'lrn:leo:templateCache',
+	/**
+	 * Map identities to the policies that they 
+	 **/
+	identities: {
+		'*': ['guest_policy'],
+		'role/admin': ['guest_policy', 'admin_policy'],
+		'user/123413': ['custom_policy']
+	},
+	/**
+	 * Define the policies that grant or deny access 
+	 **/
+	policies: {
+		//guests can only do view actions
+		'guest_policy': [{
+			Effect: "Allow",
+			Action: "view*",
+			Resource: "*",
+			Condition: {
+				IpAddress: {
+					"aws:source-ip": ["67.207.40.96", "67.163.78.93", "97.117.9.242"]
+				}
+			}
+		}],
+		//admins can do anything
+		'admin_policy': [{
+			Effect: "Allow",
+			Action: "*",
+			Resource: "*"
+		}],
+		//This user is forbidden from doing anything
+		'custom_policy': [{
+			Effect: "Deny",
+			Action: "*",
+			Resource: "*"
+		}]
+	}
+};
--- api/version/index.js
+++ api/version/index.js
@@ -1,25 +1,32 @@
 "use strict";
 let config = require("leo-config").bootstrap(require("../../leo_config.js"));
+var request = require("leo-auth")(require("../leo_auth.js"), config);
+//If I want to get info from DynamoDB instead
+//var request = require("leo-auth")(config);
 
 exports.handler = require("leo-sdk/wrappers/resource")(async function (event, context, callback) {
 	let leoaws = await config.leoaws;
 	let dynamodb = leoaws.dynamodb;
 	let settings = Object.assign({}, event);
 
-	if (event.pathParameters.version) {
-		let templates = await dynamodb.query({
-			TableName: config.resources.TemplateVersions,
-			IndexName: "v-id-index",
-			KeyConditionExpression: "v = :v",
-			ExpressionAttributeValues: {
-				":v": event.pathParameters.version
-			},
-			"ReturnConsumedCapacity": 'TOTAL'
-		});
-		callback(null, templates);
-	} else {
-		let versions = await dynamodb.scan(config.resources.Versions);
-		callback(null, versions);
-	}
-
+	return request.authorize(event, {
+		lrn: "versions",
+		action: "view"
+	}).then(async user => {
+		if (event.pathParameters.version) {
+			let templates = await dynamodb.query({
+				TableName: config.resources.TemplateVersions,
+				IndexName: "v-id-index",
+				KeyConditionExpression: "v = :v",
+				ExpressionAttributeValues: {
+					":v": event.pathParameters.version
+				},
+				"ReturnConsumedCapacity": 'TOTAL'
+			});
+			callback(null, templates);
+		} else {
+			let versions = await dynamodb.scan(config.resources.Versions);
+			callback(null, versions);
+		}
+	});
 });
