ff890f086ab9032dfadbe85ce99fbc9625397390
Steve <steve@roaringdevelopment.com>
2018-06-22 11:09:27 -0600

    upgrading to all the new stuff

---
+++ ui/js/reducers/index.js
@@ -0,0 +1,15 @@
+import {
+	combineReducers
+} from 'redux';
+
+import {
+	reducer as version
+} from "../ducks/versions.js";
+import {
+	reducer as navigation
+} from "../ducks/navigation.js";
+
+export default combineReducers({
+	version,
+	navigation
+});
